efaf135585fe58f9864fad380ed7c93297ccd776
Andreas <andreas-henschel@gmx.net>
2014-11-11 22:37:52 +0100

    Überarbeitete Version von Start.java

--- Projekt1/BenmarNr1/src/Start.java
+++ Projekt1/BenmarNr1/src/Start.java
@@ -4,6 +4,11 @@ public class Start {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Test");
+		doNothing();	
+	}
+	
+	public static void doNothing(){
+		System.out.println("nothing");
 	}
 
 }
